cc6f7a2ed5904b73306cac5319d2920b8a25deba
eggs <jonathan.eggiman@gmail.com>
2011-11-17 01:42:57 -0800

    fixed place template error

--- mysite/elTopo/views.py
+++ mysite/elTopo/views.py
@@ -5,13 +5,16 @@ from django.shortcuts import render_to_response
 
 def index(request):
 	app_places = Place.objects.all()[:4]
-	return render_to_response('elTopo/home.html', { 'app_places' : app_places })
+	return render_to_response('elTopo/home.html', { 'app_places' : app_places }
+	)
 
 def placeIndex(request):
 	app_places = Place.objects.all()
-	return render_to_response('elTopo/places.html', { 'app_places' : app_places })
+	return render_to_response('elTopo/places.html', { 'app_places' : app_places }
+	)
 	
 def place(request, place_id):
 	detail = Place.objects.get(place_id)
-	return render_to_response('elTopo/place_detail.html', {'detail' : detail })
+	return render_to_response('elTopo/place_detail.html', { 'detail' : detail }
+	)
 	
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,13 +1,14 @@
-from django.conf.urls import patterns, include, url
 from django.conf.urls.defaults import *
 from elTopo.models import *
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-	url('^$', 'elTopo.views.index'),
-	url(r'^pubs/', 'elTopo.views.placeIndex'),
-	url(r'^pubs/\d', 'elTopo.views.place'),
+urlpatterns = patterns('elTopo.views',
+	url('^$', 'index'),
+
+	url(r'^pubs/$', 'placeIndex'),
+
+	url(r'^pubs/(?P<place_id>\d+)$', 'place'),
 	
 	url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 	url(r'^admin/', include(admin.site.urls)),
